a9247ae23db466826b6400a1ed6c86feda89a547
Max Polkovnik <max-polkovnik@mail.ru>
2024-05-12 17:44:54 +0700

    Fixed database on device

--- database/src/iosMain/kotlin/com/opencritic/database/AppDatabase_InstantiateImpl.kt
+++ database/src/iosMain/kotlin/com/opencritic/database/AppDatabase_InstantiateImpl.kt
@@ -2,5 +2,7 @@ package com.opencritic.database
 
 import kotlin.reflect.KClass
 
+// Fake extension to suppress IDE error
+// Real method generated in separate sourceSet for each platform
 internal fun <T : Any> KClass<T>.instantiateImpl(): T =
     error("${this.simpleName} : Room database was not generated. Add ksp for all your targets")
--- database/src/iosMain/kotlin/com/opencritic/database/DatabaseBuilder.ios.kt
+++ database/src/iosMain/kotlin/com/opencritic/database/DatabaseBuilder.ios.kt
@@ -2,10 +2,20 @@ package com.opencritic.database
 
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import platform.Foundation.NSHomeDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSLibraryDirectory
+import platform.Foundation.NSURL
+import platform.Foundation.NSUserDomainMask
 
 fun getDatabaseBuilder(): RoomDatabase.Builder<AppDatabase> {
-    val dbFilePath = NSHomeDirectory() + "/my_room.db"
+    val libraryUrl: NSURL = NSFileManager.defaultManager()
+        .URLsForDirectory(NSLibraryDirectory, inDomains = NSUserDomainMask)
+        .last() as NSURL
+
+    val libraryPath = libraryUrl.absoluteURL?.path ?: ""
+
+    val dbFilePath = "$libraryPath/my_room.db"
+
     return Room.databaseBuilder<AppDatabase>(
         name = dbFilePath,
         factory =  { AppDatabase::class.instantiateImpl() }

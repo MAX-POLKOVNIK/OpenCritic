9b1bac0e41d175b380ae7ad6d2e9edfac4834bc9
Max Polkovnik <max-polkovnik@mail.ru>
2024-06-17 19:14:14 +0700

    Increased AGP version. Fixed requests to BE

--- api/src/commonMain/kotlin/com/opencritic/api/OpenCriticsApiImpl.kt
+++ api/src/commonMain/kotlin/com/opencritic/api/OpenCriticsApiImpl.kt
@@ -17,6 +17,7 @@ import com.opencritic.api.dto.review.ReviewSortKey
 import com.opencritic.api.dto.search.SearchItemDto
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.get
 import io.ktor.client.request.headers
 import io.ktor.http.HttpHeaders
@@ -28,75 +29,69 @@ internal class OpenCriticsApiImpl(
     private val client: HttpClient,
 ) : OpenCriticsApi {
     override suspend fun getGamePopular(): List<PopularItemDto> =
-        client.get(baseUrl + "game/popular").body()
+        client.get(baseUrl + "game/popular", headers()).body()
 
     override suspend fun getDeals(): List<DealItemDto> =
-        client.get(baseUrl + "game/deals").body()
+        client.get(baseUrl + "game/deals", headers()).body()
 
     override suspend fun getTodayReviewed(): List<ReviewedTodayGameDto> =
-        client.get(baseUrl + "game/reviewed-today").body()
+        client.get(baseUrl + "game/reviewed-today", headers()).body()
 
     override suspend fun getUpcoming(): List<ReleasedGameDto> =
-        client.get(baseUrl + "game/upcoming").body()
+        client.get(baseUrl + "game/upcoming", headers()).body()
 
     override suspend fun getRecentlyReleased(): List<ReleasedGameDto> =
-        client.get(baseUrl + "game/recently-released").body()
+        client.get(baseUrl + "game/recently-released", headers()).body()
 
     override suspend fun getHallOfFame(year: Int): List<ReleasedGameDto> =
-        client.get(baseUrl + "game/hall-of-fame/$year").body()
+        client.get(baseUrl + "game/hall-of-fame/$year", headers()).body()
 
     override suspend fun getSwitchFeatured(): FeaturedGameListDto =
-        client.get(baseUrl + "editor-sequence/switch-featured").body()
+        client.get(baseUrl + "editor-sequence/switch-featured", headers()).body()
 
     override suspend fun getXboxFeatured(): FeaturedGameListDto =
-        client.get(baseUrl + "editor-sequence/xbox-featured").body()
+        client.get(baseUrl + "editor-sequence/xbox-featured", headers()).body()
 
     override suspend fun getPlaystationFeatured(): FeaturedGameListDto =
-        client.get(baseUrl + "editor-sequence/playstation-featured").body()
+        client.get(baseUrl + "editor-sequence/playstation-featured", headers()).body()
 
     override suspend fun getGame(gameId: Long): GameDetailsDto =
-        client.get(baseUrl + "game/${gameId}").body()
+        client.get(baseUrl + "game/${gameId}", headers()).body()
 
     override suspend fun getGameReviews(gameId: Long, skip: Int, sort: ReviewSortKey): List<ReviewDto> =
-        client.get(baseUrl + "reviews/game/$gameId/?skip=$skip&sort=${sort.queryValue}").body()
+        client.get(baseUrl + "reviews/game/$gameId/?skip=$skip&sort=${sort.queryValue}", headers()).body()
 
     override suspend fun search(criteria: String): List<SearchItemDto> =
-        client.get(baseUrl + "meta/search?criteria=$criteria") {
-            headers {
-                append(HttpHeaders.CacheControl, "no-cache")
-                append(HttpHeaders.Host, "api.opencritic.com")
-                append(HttpHeaders.Origin, "https://opencritic.com")
-                append(HttpHeaders.Referrer, "https://opencritic.com/")
-                append(HttpHeaders.Accept, "application/json")
-                append(HttpHeaders.UserAgent, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:124.0) Gecko/20100101 Firefox/124.0")
-                append("Sec-Fetch-Site", "same-site")
-                append("Sec-Fetch-Dest", "empty")
-                append("Sec-Fetch-Mode", "cors")
-            }
-        }.body()
+        client.get(baseUrl + "meta/search?criteria=$criteria", headers()).body()
 
     override suspend fun getOutlet(outletId: Int): OutletDto =
-        client.get(baseUrl + "outlet/$outletId").body()
+        client.get(baseUrl + "outlet/$outletId", headers()).body()
 
     override suspend fun getOutletReviews(
         outletId: Int,
         skip: Int,
         sort: ReviewSortKey
     ): List<ReviewDto> =
-        client.get(baseUrl + "reviews/outlet/$outletId/?skip=$skip&sort=${sort.queryValue}").body()
+        client.get(
+            baseUrl + "reviews/outlet/$outletId/?skip=$skip&sort=${sort.queryValue}",
+            headers()
+        ).body()
 
     override suspend fun getAuthor(authorId: Int): AuthorDto =
-        client.get(baseUrl + "author/$authorId").body()
+        client.get(baseUrl + "author/$authorId", headers()).body()
 
     override suspend fun getAuthorReviews(
         authorId: Int,
         skip: Int,
         sort: ReviewSortKey
     ): List<ReviewDto> =
-        client.get(baseUrl + "reviews/author/$authorId/?skip=$skip&sort=${sort.queryValue}").body()
+        client.get(
+            baseUrl + "reviews/author/$authorId/?skip=$skip&sort=${sort.queryValue}",
+            headers()
+        ).body()
 
     override suspend fun getPlatforms(): List<PlatformDto> =
-        client.get(baseUrl + "platform").body()
+        client.get(baseUrl + "platform", headers()).body()
 
     override suspend fun getGames(
         platformShortName: String,
@@ -104,8 +99,25 @@ internal class OpenCriticsApiImpl(
         sort: GameSortKey,
         skip: Int,
     ): List<BrowseGameDto> =
-        client.get(baseUrl + "game?platforms=$platformShortName&skip=$skip&sort=${sort.queryValue}&time=${time.queryKey}").body()
+        client.get(
+            baseUrl + "game?platforms=$platformShortName&skip=$skip&sort=${sort.queryValue}&time=${time.queryKey}",
+            headers()
+        ).body()
 
     override suspend fun getReviewedThisWeek(): List<BrowseGameDto> =
-        client.get(baseUrl + "game/reviewed-this-week").body()
+        client.get(baseUrl + "game/reviewed-this-week", headers()).body()
+
+    private fun headers(): HttpRequestBuilder.() -> Unit = {
+        headers {
+            append(HttpHeaders.CacheControl, "no-cache")
+            append(HttpHeaders.Host, "api.opencritic.com")
+            append(HttpHeaders.Origin, "https://opencritic.com")
+            append(HttpHeaders.Referrer, "https://opencritic.com/")
+            append(HttpHeaders.Accept, "application/json")
+            append(HttpHeaders.UserAgent, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:124.0) Gecko/20100101 Firefox/124.0")
+            append("Sec-Fetch-Site", "same-site")
+            append("Sec-Fetch-Dest", "empty")
+            append("Sec-Fetch-Mode", "cors")
+        }
+    }
 }

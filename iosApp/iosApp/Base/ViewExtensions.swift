b7151a94662cf4c2b14d7b0e34c4aa78243e92b4
Max Polkovnik <max-polkovnik@mail.ru>
2024-08-05 11:57:06 +0700

    Changed card implementation on iOS

--- iosApp/iosApp/Base/ViewExtensions.swift
+++ iosApp/iosApp/Base/ViewExtensions.swift
@@ -12,38 +12,18 @@ import UIKit
 import shared
 
 extension View {
-    /// Applies the given transform if the given condition evaluates to `true`.
-    /// - Parameters:
-    ///   - condition: The condition to evaluate.
-    ///   - transform: The transform to apply to the source `View`.
-    /// - Returns: Either the original `View` or the modified `View` if the condition is `true`.
-    @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
-        if condition {
-            transform(self)
-        } else {
-            self
-        }
-    }
-    
     func card(radius: CGFloat = 8) -> some View {
-        if UITraitCollection.current.userInterfaceStyle == .dark {
-            AnyView(
-                self.clipShape(.rect(cornerRadius: radius))
-                    .background(
-                        RoundedRectangle(cornerRadius: radius)
-                            .fill(Color(red: 30 / 255, green: 30 / 255, blue: 30 / 255))
-                    )
-            )
-        } else {
-            AnyView(
-                self.clipShape(.rect(cornerRadius: radius))
-                    .background(
-                        RoundedRectangle(cornerRadius: radius)
-                            .fill(UIColor.systemBackground.toColor())
-                            .shadow(color: .gray, radius: radius / 4, x: 0, y: radius / 4)
-                    )
-            )
-        }
+        let color = UITraitCollection.current.userInterfaceStyle == .dark
+            ? Color(red: 30 / 255, green: 30 / 255, blue: 30 / 255)
+            : Color(red: 245 / 255, green: 245 / 255, blue: 245 / 255)
+        
+        return AnyView(
+            self.clipShape(.rect(cornerRadius: radius))
+                .background(
+                    RoundedRectangle(cornerRadius: radius)
+                        .fill(color)
+                )
+        )
     }
     
     func navigationTitle(_ textSource: TextSource) -> some View {

05ec22f8d6a4bc4f343661423c16c4a74d5564e9
Max Polkovnik <max-polkovnik@mail.ru>
2024-08-06 18:05:07 +0700

    App version increased

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.opencritic.app.android"
         minSdk = 28
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
     buildFeatures {
         compose = true
